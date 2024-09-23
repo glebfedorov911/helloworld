3e6ca7ad3fa6570d7f5adcc4322fd7d221ebf845
glebfedorov911 <fedorov22134@gmail.com>
2024-09-23 22:38:45 +0300

    edit array

--- practice1/laba5.cpp
+++ practice1/laba5.cpp
@@ -15,14 +15,14 @@ using std::find;
 int main()
 {
     int N = 10;
-    int A[] = {0, 0, 0, 0, 0, 1, 0, 0, 10};
-    int B[] = {8, 1, 6, 5, 2, 7, 9, 3, 4, 10};
+    int A[N] = {0, 35, 30, 12, 0, 11, 1, 2, 10, 0};
+    int B[N] = {8, 1, 6, 5, 2, 7, 9, 3, 4, 10};
 
     auto F = [&A, &B, N]() {
         int k = count_if(A, A+N, [&B, N](int num) {
             return find(B, B+N, num) != (B+N);
         });
-        int percent = (double) k / double(N) * 100; 
+        int percent = (double) k / double(N) * 100;
         return percent >= 30;
     }();
 
