3535f3f379fa39a2e7e76e900d162ad5570c4795
Gleb <fedorov22134@gmail.com>
2024-10-06 23:07:42 +0300

    Update laba7again.cpp

--- practice1/laba7again.cpp
+++ practice1/laba7again.cpp
@@ -1,132 +1,141 @@
+#define _CRT_SECURE_NO_WARNINGS 
+
 #include <iostream>
+#include <cstring>
 
 using std::cout;
 using std::endl;
-using std::string;
+using std::strlen;
 
-int count_of_finding_word(string& str, int len);
-string* sub_words(string& str, int cnt);
-int smallest_word_length(string* subStr, int length);
-bool check_word(string* s1, string* s2, int sizeS1, int sizeS2, int smallest_len);
-string delete_spaces(string s);
+int count_of_finding_word(const char* str, int len);
+const char** sub_words(const char* str, int cnt);
+int smallest_word_length(const char** subStr, int length);
+bool check_word(const char** s1, const char** s2, int sizeS1, int sizeS2, int smallest_len);
+const char* delete_spaces(const char* s);
 
-/* 
+/*
 Федоров 24ВП1
-Присвоить переменной F значение true, если в предложении S2 есть хотя бы одно слово предложения S1 
+Присвоить переменной F значение true, если в предложении S2 есть хотя бы одно слово предложения S1
 и длина которого превышает длину самого короткого слова S1, в противном случае присвоить переменной F - false
 */
 
-int main()
-{
-	string s1{ "Hello world" };	 // 0
-	// string s1{ "Hello Gleb" };	 // 1
-	string s2{ "Hello Teacher" };
-	s1 = delete_spaces(s1);
-	s2 = delete_spaces(s2);
-	int length1 = s1.size();
-	int length2 = s2.size();
-	int cnt1 = count_of_finding_word(s1, length1);
-	int cnt2 = count_of_finding_word(s2, length2);
+int main() {
+    const char* s1{ "Hello World" };
+    const char* s2{ "Hello Teacher" };
+    s1 = delete_spaces(s1);
+    s2 = delete_spaces(s2);
 
-	string* stringArray1 = sub_words(s1, cnt1);
-	int smallestWordS1 = smallest_word_length(stringArray1, cnt1);
+    int length1 = strlen(s1);
+    int length2 = strlen(s2);
 
-	string* stringArray2 = sub_words(s2, cnt2);
+    int cnt1 = count_of_finding_word(s1, length1);
+    int cnt2 = count_of_finding_word(s2, length2);
 
-	bool f = check_word(stringArray1, stringArray2, cnt1, cnt2, smallestWordS1);
+    const char** stringArray1 = sub_words(s1, cnt1);
+    int smallestWordS1 = smallest_word_length(stringArray1, cnt1);
 
-	cout << "F: " << f;
+    const char** stringArray2 = sub_words(s2, cnt2);
 
-	return 0;
+    bool f = check_word(stringArray1, stringArray2, cnt1, cnt2, smallestWordS1);
+
+    cout << "F: " << f;
+
+    return 0;
 }
 
-string delete_spaces(string s)
-{
-	string result;
-	bool inWord = false;
+const char* delete_spaces(const char* s) {
+    char* result = new char[strlen(s) + 1];
+    bool inWord = false;
+    int index = 0;
 
-	for (int i = 0; i < s.size(); i++)
-	{
-		if (s[i] != ' ')
-		{
-			result += s[i];
-			inWord = true;
-		}
-		else if (inWord)
-		{
-			result += ' ';
-			inWord = false;
-		}
-	}
+    for (int i = 0; i < strlen(s); i++) {
+        if (s[i] != ' ') {
+            result[index++] = s[i];
+            inWord = true;
+        }
+        else if (inWord) {
+            result[index++] = ' ';
+            inWord = false;
+        }
+    }
 
-	if (!result.empty() && result.back() == ' ') result.pop_back();
+    if (index > 0 && result[index - 1] == ' ') {
+        index--;
+    }
 
-	return result;
+    result[index] = '\0';
+
+    return result;
 }
 
-int count_of_finding_word(string& str, int len)
-{
-	if (len == 0) return 0;
+int count_of_finding_word(const char* str, int len) {
+    if (len == 0) return 0;
 
-	int cnt = 0;
-	bool inWord = false;
+    int cnt = 0;
+    bool inWord = false;
 
-	for (int i = 0; i < len; i++)
-	{
-		if (str[i] != ' ')
-		{
-			if (!inWord)
-			{
-				inWord = true;
-				cnt++;
-			}
-		}
-		else inWord = false;
-	}
-	return cnt;
+    for (int i = 0; i < len; i++) {
+        if (str[i] != ' ') {
+            if (!inWord) {
+                inWord = true;
+                cnt++;
+            }
+        }
+        else {
+            inWord = false;
+        }
+    }
+    return cnt;
 }
 
-string* sub_words(string& str, int cnt)
-{
-	string* stringArray = new string[cnt];
-	string tempStr;
-	char delimiter = ' ';
+const char** sub_words(const char* str, int cnt) {
+    const char** stringArray = new const char* [cnt]; 
+    const char* temp = str; 
+    int index = 0; 
 
-	for (int i = 0; i < cnt; i++)
-	{
-		tempStr = str.substr(0, str.find(delimiter));
-		if (!tempStr.empty()) stringArray[i] = tempStr;
-		else stringArray[i] = str;
-		str = str.substr(str.find(delimiter) + 1);
-	}
+    while (*temp) { 
+        while (*temp == ' ') {
+            temp++;
+        }
+        if (*temp) {
+            const char* start = temp; 
+            while (*temp && *temp != ' ') {
+                temp++;
+            }
 
-	return stringArray;
+            int length = temp - start;
+
+            char* word = new char[length + 1];
+
+            for (int i = 0; i < length; i++) {
+                word[i] = start[i]; 
+            }
+            word[length] = '\0'; 
+
+            stringArray[index++] = word;
+        }
+    }
+
+    return stringArray;
 }
 
-int smallest_word_length(string* subStr, int len)
-{
-	int k = subStr[0].size();
-	for (int i = 1; i < len; i++)
-	{
-		if (!subStr[i].empty() && subStr[i] != " ") {
-			k = subStr[i].size() < k ? subStr[i].size() : k;
-		}
-	}
-	return k;
+int smallest_word_length(const char** subStr, int len) {
+    int k = strlen(subStr[0]);
+    for (int i = 0; i < len; i++) {
+        if (subStr[i] != nullptr && subStr[i][0] != '\0') {
+            k = strlen(subStr[i]) < k ? strlen(subStr[i]) : k;
+        }
+    }
+    return k;
 }
 
-bool check_word(string* s1, string* s2, int sizeS1, int sizeS2, int smallest_len)
-{
-	for (int i = 0; i < sizeS1; i++)
-	{
-		for (int j = 0; j < sizeS2; j++)
-		{
-			if (s1[i] == s2[j] && s2[j].size() > smallest_len)
-			{
-				return true;
-			}
-		}
-
-	}
-	return false;
+bool check_word(const char** s1, const char** s2, int sizeS1, int sizeS2, int smallest_len) {
+    for (int i = 0; i < sizeS1; i++) {
+        for (int j = 0; j < sizeS2; j++) {
+            if (*s1[i] == *s2[j] && strlen(s2[j]) > smallest_len) {
+                return true;
+            }
+        }
+    }
+    return false;
 }
